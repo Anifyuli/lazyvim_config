4d0460997436e46783e427aec0caec897de83c7f
Anifyuli <anifyuliansyah@gmail.com>
2024-01-12 08:49:51 +0700

    Add some config to boost UX this configs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
+local g = vim.g
 
+-- Using Vim built-in options
 opt.wrap = true
+
+-- Configure dart-vim-plugin
+g.dart_style_guide = 2
+g.dart_format_on_save = true
---
+++ lua/plugins/coding.lua
@@ -0,0 +1,12 @@
+-- This is config files for coding stuff, include override default config of LazyVim and add new formatter or something like that
+return {
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+  {
+    "dart-lang/dart-vim-plugin",
+  },
+}
