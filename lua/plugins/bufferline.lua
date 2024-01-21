4a67f34f0af25e683feda39cd1363b65ca97b122
Anifyuli <anifyuliansyah@gmail.com>
2024-01-21 21:22:35 +0700

    Repair my bufferline configs

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -13,23 +13,27 @@ return {
     optional = true,
     opts = function()
       local Offset = require("bufferline.offset")
-      if not Offset.edgy and package.loaded.edgy then
-        local layout = require("edgy.config").layout
-        local ret = { left = "", left_size = 0, right = "", right_size = 0 }
-        for _, pos in ipairs({ "left", "right" }) do
-          local sb = layout[pos]
-          if sb and #sb.wins > 0 then
-            local title = " Sidebar" .. string.rep(" ", sb.bounds.width - 8)
-            ret[pos] = "%#EdgyTitle#" .. title .. "%*" .. "%#WinSeparator#│%*"
-            ret[pos .. "_size"] = sb.bounds.width
+      if not Offset.edgy then
+        local get = Offset.get()
+        if package.loaded.edgy then
+          local layout = require("edgy.config").layout
+          local ret = { left = "", left_size = 0, right = "", right_size = 0 }
+          for _, pos in ipairs({ "left", "right" }) do
+            local sb = layout[pos]
+            if sb and #sb.wins > 0 then
+              local title = string.rep(" ", sb.bounds.width)
+              ret[pos] = "%#EdgyTitle#" .. title .. "%*" .. "%#WinSeparator#│%*"
+              ret[pos .. "_size"] = sb.bounds.width
+            end
+          end
+          ret.total_size = ret.left_size + ret.right_size
+          if ret.total_size > 0 then
+            return ret
           end
         end
-        ret.total_size = ret.left_size + ret.right_size
-        if ret.total_size > 0 then
-          return ret
-        end
+        return get()
       end
-      return Offset.get()
+      Offset.edgy = true
     end,
   },
 }
